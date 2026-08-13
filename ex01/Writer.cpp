55f7a171446a2cdc4742d156ae85b2038dd5ce52
NeXus1183 <nicjianhao@gmail.com>
2026-08-13 17:36:56 +0800

    updated ex01 again

--- ex01/Writer.cpp
+++ ex01/Writer.cpp
@@ -4,7 +4,6 @@ void Writer::write(std::string path, std::string packed, unsigned sum)
 {
     std::ofstream toWrite(path);
     toWrite << packed << "\n";
-    toWrite << sum << "\n";
     toWrite.close();
     std::cout << "write     ->" << path << " (" << sum << " bytes)" << std::endl;
 }
